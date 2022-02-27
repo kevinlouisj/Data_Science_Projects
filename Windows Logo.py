8df570447201fbf6f318b5035c37ed99af7e3b8f
kevinlouisj <kevinlouis9806@gmail.com>
2022-02-27 20:02:28 +0530

    Logo

---
+++ Windows Logo.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from turtle import *
+
+
+# In[2]:
+
+
+speed(2)
+bgcolor("black")
+penup()
+goto(-50,60)
+pendown()
+color("#00adef")
+begin_fill()
+goto(100,100)
+goto(100,-100)
+goto(-50,-60)
+goto(-50,60)
+end_fill()
+color("black")
+goto(15,100)
+color("black")
+width(10)
+goto(15,-100)
+penup()
+goto(15,-100)
+penup
+goto(100,0)
+pendown()
+goto(-100,0)
+done
+
+
+# credits to the owner
